9d6ea44b3e47f169b093ee7de25fbcbeec796186
Roman Beltyukov <maybe-hello-world@users.noreply.github.com>
2017-09-12 14:37:50 +0300

    prng.py - Added pseudorandom number generator grapher.py - added xlabel and ylabel, added points for graph, changed range() to frange() to allow not-int x-values

--- grapher.py
+++ grapher.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import pylab
 
 axs = {}
 
@@ -6,11 +7,22 @@ def init(figure_number=1, XKCD=False):
     if XKCD is True: plt.xkcd()
     plt.figure(figure_number)
 
-def set_subplot(subplot_number, func, begin, end, xmin=None, xmax=None, ymin=None, ymax=None, grid=True, subplot_x_number=2, subplot_y_number=2):
+def set_subplot(subplot_number, func, begin, end, step=1,
+                xmin=None, xmax=None, ymin=None, ymax=None,
+                grid=True, subplot_x_number=2, subplot_y_number=2,
+                xlabel = None, ylabel = None, text_points = []):
     axs[subplot_number - 1] = plt.subplot(subplot_x_number, subplot_y_number, subplot_number)
+
     if xmin is not None and xmax is not None: axs[subplot_number - 1].set_xlim([xmin, xmax])
     if ymin is not None and ymax is not None: axs[subplot_number - 1].set_ylim([ymin, ymax])
-    plt.plot([func(x) for x in range(begin, end)])
+    if xlabel is not None: plt.xlabel(xlabel)
+    if ylabel is not None: plt.ylabel(ylabel)
+
+    x_ar = pylab.frange(begin, end, step)
+    plt.plot(x_ar, [func(x) for x in x_ar])
+
+    for point in text_points:
+        plt.text(point[0], point[1], point[2])
     plt.grid(grid)
 
 
---
+++ prng.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+import math
+
+class PRNG:
+    """
+    Simple pseudorandom number generator
+    """
+    prev = 0
+
+    def __init__(self, seed = None):
+        """
+        Initialize prng
+
+        :param seed: seed for initialization. Multiple initializations with similar seed will return similar sequences of number
+        """
+        if seed is None:
+            self.prev = datetime.now().microsecond
+        else:
+            self.prev = math.fabs(seed)
+
+
+    def next(self, left = 0, right = 1):
+        """
+        Get next pseudorandom value
+
+        :param left: left boundary of possible values
+        :param right: right boundary of possible values (does not include value itself)
+        :return: pseudorandom float number in [left; right)
+        :raise ValueError
+        """
+        if left > right: raise ValueError("Left boundary must be <= right")
+
+        self.prev = ((self.prev*148878.553) % 100) / 100
+        return left + self.prev * (right - left)
