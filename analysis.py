7a8b089923533628edbd7a4eeeb2093393b34d3e
Roman Beltyukov <maybe-hello-world@users.noreply.github.com>
2017-09-21 23:27:06 +0300

    Re-implemented auto-correlation function, added function notation, fixed density function

--- analysis.py
+++ analysis.py
@@ -3,10 +3,11 @@ Yeah, it's forbidden to use built-in function or packages
 """
 import math
 
-def mean(arr):
+def mean(arr) -> float:
     """
     Returns average value of array
 
+    :rtype: float
     :param arr: array of values
     :return: average value
     """
@@ -15,10 +16,11 @@ def mean(arr):
         sm += i
     return sm / len(arr)
 
-def square_mean(arr):
+def square_mean(arr) -> float:
     """
     Returns squared mean (not mean^2, but sum of i^2)
 
+    :rtype: float
     :param arr: array of values
     :return: squared mean value
     """
@@ -27,37 +29,41 @@ def square_mean(arr):
         sm += i**2
     return sm / len(arr)
 
-def stand_deviation(arr):
+def root_mean_square(arr) -> float:
     """
     Returns standard deviation of array
 
+    :rtype: float
     :param arr: array of values
     :return: standard deviation of all values in array
     """
     return math.sqrt(square_mean(arr))
 
-def variance(arr):
+def variance(arr) -> float:
     """
     Calculates dispersion of values in array
 
+    :rtype: float
     :param arr: array of values
     :return: dispersion value for the array
     """
     return moment(arr, 2)
 
-def sqrt_variance(arr):
+def sqrt_variance(arr) -> float:
     """
     Calculates sqrt of variance of values in array
 
+    :rtype: float
     :param arr: array of values
     :return: square root of variance
     """
     return math.sqrt(variance(arr))
 
-def moment(arr, ordinal):
+def moment(arr, ordinal: int) -> float:
     """
     Calculates ordinal moment of array
 
+    :rtype: float
     :param arr: array of values
     :param ordinal: ordinal of moment
     :return: ordinal moment value
@@ -68,28 +74,31 @@ def moment(arr, ordinal):
         d += (i - av) ** ordinal
     return d / len(arr)
 
-def skewness(arr):
+def skewness(arr) -> float:
     """
     Calculates skewness (assymetry coeff) for values
 
+    :rtype: float
     :param arr: array of function's values
     :return: skewness value
     """
     return moment(arr, 3) / (sqrt_variance(arr) ** 3)
 
-def kurtosis(arr):
+def kurtosis(arr) -> float:
     """
     Calculates kurtosis (exscess coeff) for function's values
 
+    :rtype: float
     :param arr: array of values
     :return: kurtosis value
     """
     return moment(arr, 4) / (sqrt_variance(arr) ** 4)
 
-def density(arr, M):
+def density(arr, M: int) -> list:
     """
     Calculate probability density for array of values
 
+    :rtype: float
     :param arr: array of function's values
     :param M: number of intervals
     :return: array of intervals (len(arr) = M) with number of values in each interval
@@ -102,33 +111,31 @@ def density(arr, M):
 
     # Increment element in list that corresponds to given value in range [lMin, lMax]
     for i in arr:
-        ans[int(math.floor((i - lMin) / divisor))] += 1
+        try:
+            ans[int(math.floor((i - lMin) / divisor))] += 1
+        except IndexError:
+            # Last range includes max values
+            ans[-1] += 1
 
     # Divide each element by arr length in order to get probability
     return [i / len(arr) for i in ans]
 
-
-def autocorrelation(arr, lag):
+def autocorrelation(arr, lag: int) -> float:
     """
     Calculates auto-correlation value for given lag for function f(x)
 
+    :rtype: float
     :param arr: array of values
     :param lag: lag for auto-correlation (value from 0 to N-1)
     :return: auto-correlation value for given lag for values in array
     """
-    avg = mean(arr)
-    lSum = 0
-    for i in range(0, len(arr) - lag - 1):
-        lSum += (arr[i] - avg) * (arr[i + lag] - avg)
+    return crosscorrelation(arr, arr, lag)
 
-    return lSum / (len(arr) - lag)
-
-
-
-def crosscorrelation(arr_f, arr_g, lag):
+def crosscorrelation(arr_f, arr_g, lag: int) -> float:
     """
     Calculates cross-correlation value for given lag for functions f(x) and g(y)
 
+    :rtype: float
     :param arr_f: array of values for f(x) function
     :param arr_g: array of values for g(x) function
     :param lag: lag for correlation (value from 0 to N-1)
