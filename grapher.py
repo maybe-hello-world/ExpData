725e259f53e086d4d685d59ffd95fd4503decdb8
Roman Beltyukov <maybe-hello-world@users.noreply.github.com>
2017-10-02 12:51:48 +0300

    Small improvements:  - Spaces to tabs  - Removed unnecessary import  - some params for subplot added  - annotations fixed

--- grapher.py
+++ grapher.py
@@ -1,33 +1,62 @@
 import matplotlib.pyplot as plt
-import pylab
 
-axs = {}
+axs = []
 
 def init(figure_number=1, XKCD=False):
-    if XKCD is True: plt.xkcd()
-    plt.figure(figure_number)
+	if XKCD is True: plt.xkcd()
+	plt.figure(figure_number)
 
-def set_subplot(subplot_number, y_arr, begin, end, step=1,
-                xmin=None, xmax=None, ymin=None, ymax=None,
-                grid=True, subplot_x_number=2, subplot_y_number=2,
-                xlabel = None, ylabel = None, xticks = None, yticks = None,
-                text_points = []):
-    axs[subplot_number - 1] = plt.subplot(subplot_x_number, subplot_y_number, subplot_number)
+def set_subplot(subplot_number, y_arr, x_arr,
+				xmin=None, xmax=None, ymin=None, ymax=None,
+				grid=True, subplot_x_number=2, subplot_y_number=2,
+				xlabel = None, ylabel = None, title = None,
+				xticks = None, yticks = None,
+				annotations = None, bar = False):
+	"""
+	Define subplot in graph
 
-    if xmin is not None and xmax is not None: axs[subplot_number - 1].set_xlim([xmin, xmax])
-    if ymin is not None and ymax is not None: axs[subplot_number - 1].set_ylim([ymin, ymax])
-    if xlabel is not None: plt.xlabel(xlabel)
-    if ylabel is not None: plt.ylabel(ylabel)
-    if xticks is not None: plt.xticks(xticks[0], xticks[1])
-    if yticks is not None: plt.yticks(yticks[0], yticks[1])
+	:param subplot_number: Number of subplot
+	:param y_arr: Array of function's values
+	:param x_arr: Array of function's arguments
+	:param xmin: Minimum value on X axis
+	:param xmax: Maximum value on X asix
+	:param ymin: Minimum value on Y axis
+	:param ymax: Maximum value on Y axis
+	:param grid: If grid on the subplot needed
+	:param subplot_x_number: Number of subplots in row
+	:param subplot_y_number: Number of subplots in column
+	:param xlabel: Label of X axis
+	:param ylabel: Label of Y axis
+	:param title: Title of subplot
+	:param xticks: List of 2 lists: 1 contains tick locations (usually list of numbers from start to end), 2 contains tick names
+	:param yticks: List of 2 lists: 1 contains tick locations (usually list of numbers from start to end), 2 contains tick names
+	:param annotations: List of annotations. Each element is a list of next elements:
+		0 - text of annotation,
+		1 - tuple (x, y) of desired point of annotation,
+		2 - tuple (x, y) as annotation text coordinates
+	:param bar: Bool param if bars or graph is needed
+	:return: Nothing
+	"""
+	axs[subplot_number - 1] = plt.subplot(subplot_x_number, subplot_y_number, subplot_number)
 
-    x_ar = pylab.frange(begin, end, step)
-    plt.plot(x_ar, y_arr)
+	if xmin is not None and xmax is not None: axs[subplot_number - 1].set_xlim([xmin, xmax])
+	if ymin is not None and ymax is not None: axs[subplot_number - 1].set_ylim([ymin, ymax])
+	if xlabel is not None: plt.xlabel(xlabel)
+	if ylabel is not None: plt.ylabel(ylabel)
+	if title is not None: plt.title(title)
+	if xticks is not None: plt.xticks(xticks[0], xticks[1])
+	if yticks is not None: plt.yticks(yticks[0], yticks[1])
 
-    for point in text_points:
-        plt.text(point[0], point[1], point[2])
-    plt.grid(grid)
+	if bar:
+		plt.bar(x_arr, y_arr)
+	else:
+		plt.plot(x_arr, y_arr)
+
+	if annotations is not None:
+		for point in annotations:
+			plt.annotate(str(point[0]), xy=point[1], arrowprops=dict(arrowstyle='->'), xytext=point[2])
+		plt.grid(grid)
 
 
 def show():
-    plt.show()
+	plt.show()
