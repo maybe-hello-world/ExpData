079e5835e66e2d7968dcbf4a4d5305aa77b92e46
Roman Beltyukov <maybe-hello-world@users.noreply.github.com>
2017-10-30 14:06:06 +0300

    Init.py implemented

---
+++ analysis/__init__.py
@@ -0,0 +1,8 @@
+__all__ = ['examples', 'FT', 'preprocessing', 'statistics', 'trends', 'impulse_response']
+
+import analysis.examples
+import analysis.FT
+import analysis.preprocessing
+import analysis.statistics
+import analysis.trends
+import analysis.impulse_response
