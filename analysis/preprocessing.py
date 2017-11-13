0cf3dc04c7abfbd84dbf30a8b71dd8ba836947ee
Roman Beltyukov <maybe-hello-world@users.noreply.github.com>
2017-11-13 15:00:48 +0300

    anti_shift implemented

--- analysis/preprocessing.py
+++ analysis/preprocessing.py
@@ -1,6 +1,10 @@
 """
 Some data preprocessing functions
 """
+from analysis.statistics import mean, std
+import math
+
+
 def normalize(arr) -> None:
 	"""
 	Normalize data in given array
@@ -28,3 +32,55 @@ def normalize_max(arr) -> None:
 
 	for i in range(len(arr)):
 		arr[i] = arr[i] / maxX
+
+def anti_shift(arr) -> None:
+	"""
+	Shift data to zero mean
+
+	:param arr: Array of data
+	"""
+	m = mean(arr)
+	for i in range(len(arr)):
+		arr[i] -= m
+
+def anti_spike(arr) -> None:
+	"""
+	Delete spikes that are more than mean + 4 sigmas
+
+	:param arr: array of data with spikes
+	"""
+	avg = mean(arr)
+	sigma = std(arr)
+	for x in range(len(arr)):
+		if math.fabs(arr[x]) > avg + 4 * sigma:
+			if x == 0:
+				arr[x] = (arr[x + 1] + arr[x + 2]) / 2
+			elif x == len(arr) - 1:
+				arr[x] = (arr[x - 2] + arr[x - 1]) / 2
+			else:
+				arr[x] = (arr[x-1] + arr[x + 1]) / 2
+
+def anti_trend(arr, window_width = None) -> None:
+	"""
+	Use floating window to remove trends from data
+
+	:param arr: array of values
+	:param window_width: width of window
+	"""
+	if window_width is None :
+		window_width = int(len(arr) / 100)
+
+	counter = int(math.floor(len(arr) / window_width))
+	for i in range(counter):
+		mean_v = mean(arr[i * window_width : (i + 1) * window_width])
+		for x in range(window_width):
+			arr[i * window_width + x] -= mean_v
+
+	# Resize last values
+	if window_width * counter == len(arr):
+		return
+
+	mean_v = mean(arr[window_width * counter:])
+	for x in range(window_width * counter, len(arr)):
+		arr[x] -= mean_v
+
