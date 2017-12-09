47cf55477259c6fbbd114c4f319bd08d6bf4dbb8
Roman Beltyukov <maybe-hello-world@users.noreply.github.com>
2017-12-09 22:07:09 +0300

    Speed optimizations

--- analysis/impulse_response.py
+++ analysis/impulse_response.py
@@ -1,7 +1,10 @@
 """
 Module that implements impulse response function
 """
+import numpy as np
+from numba import jit, float64, int64, double
 
+@jit(float64[:](float64[:], float64[:]))
 def process(x_values, h_values) -> list:
 	"""
 	Generate impulse response of system with given delta function values and input function values
@@ -13,13 +16,17 @@ def process(x_values, h_values) -> list:
 	N = len(x_values)
 	m = len(h_values)
 
-	answer = []
+	x_values_view = np.array(x_values, dtype=np.double)
+	h_values_view = np.array(h_values, dtype=np.double)
+	answer = np.array([0] * (N + m), dtype=np.double)
+
 	for k in range(N + m):
-		answer.append(__step(x_values, h_values, k))
+		answer[k] = __step(x_values_view, h_values_view, k, N, m)
 
-	return answer
+	return list(answer.tolist())
 
-def __step(x_values, h_values, step_number: int) -> float:
+@jit(float64(double[:], double[:], int64, int64, int64))
+def __step(x_values: np.ndarray, h_values: np.ndarray, step_number: int, N: int, M: int) -> float:
 	"""
 	Single step of Impulse Response process
 
@@ -28,9 +35,8 @@ def __step(x_values, h_values, step_number: int) -> float:
 	:param step_number: step number
 	:return: step value
 	"""
+
 	Y_k = 0
-	M = len(h_values)
-	N = len(x_values)
 
 	for l in range(M):
 		if step_number >= l and step_number - l < N:
