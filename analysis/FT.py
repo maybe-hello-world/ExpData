7c23161e0517aa23f27ab5cfd3edc8f331a5de0a
Roman Beltyukov <maybe-hello-world@users.noreply.github.com>
2017-12-09 22:07:35 +0300

    Speed optimizations

--- analysis/FT.py
+++ analysis/FT.py
@@ -2,7 +2,8 @@
 Fourier transformation module
 """
 
-import math
+import numpy as np
+from numba import jit, float64, int64
 
 
 class FT_result:
@@ -20,10 +21,10 @@ class FT_result:
 	frequencies: list
 	Re: list
 	Im: list
-	deltaF: int
-	borderF: int
+	deltaF: float64
+	borderF: float64
 
-
+@jit
 def fourier_transform(arr, deltaT: float) -> FT_result:
 	"""
 	Performs Fourier transformation over some function's values
@@ -35,24 +36,26 @@ def fourier_transform(arr, deltaT: float) -> FT_result:
 	"""
 	res = FT_result()
 
-	res.frequencies = []
-	res.Re = []
-	res.Im = []
+	N = len(arr)
+	Res = np.array([0] * N, dtype=np.double)
+	Ims = np.array([0] * N, dtype=np.double)
+	Cs = np.array([0] * N, dtype=np.double)
+	t_arr = np.array(arr)
 
 	# Calculate FT
-	for n in range(len(arr)):
-		Re, Im, C = __FT_step(n, arr)
-		res.Re.append(Re)
-		res.Im.append(Im)
-		res.frequencies.append(C)
+	for n in range(N):
+		Res[n], Ims[n], Cs[n] = __FT_step(n, t_arr)
 
+	res.Re = Res.tolist()
+	res.Ims = Ims.tolist()
+	res.frequencies = Cs.tolist()
 	res.borderF = __calculate_borderF(deltaT)
-	res.deltaF = __calculate_deltaF(res.borderF, len(arr))
+	res.deltaF = __calculate_deltaF(res.borderF, N)
 
 	return res
 
-
-def __FT_step(n: int, arr) -> (float, float, float):
+@jit(parallel=True)
+def __FT_step(n: int, arr: np.ndarray) -> (float, float, float):
 	"""
 	Internal step of Fourier transformation
 
@@ -60,23 +63,26 @@ def __FT_step(n: int, arr) -> (float, float, float):
 	:param arr: array of values
 	:return: Re value, Im value and C value in terms of Fourier Transformation algo
 	"""
-	Re = 0
-	Im = 0
 	N = len(arr)
 
-	for k in range(N):
-		Re += arr[k] * math.cos(2 * math.pi * n * k / N)
-	Re /= N
+	idxs = np.array([i for i in range(N)])
 
-	for k in range(N):
-		Im += arr[k] * math.sin(2 * math.pi * n * k / N)
-	Im /= N
+	Re = np.divide(
+		np.sum(arr * np.cos(2 * np.pi * n * idxs / N)),
+		N
+	)
 
-	C = math.sqrt(Re ** 2 + Im ** 2)
+	Im = np.divide(
+		np.sum(arr * np.sin(2 * np.pi * n * idxs / N)),
+		N
+	)
+
+	C = np.sqrt(Re ** 2 + Im ** 2)
 
 	return Re, Im, C
 
 
+@jit(float64(float64, int64))
 def __calculate_deltaF(borderF: float, N: int) -> float:
 	"""
 	Calculate \delta F (how long is one step given FT results)
@@ -88,6 +94,7 @@ def __calculate_deltaF(borderF: float, N: int) -> float:
 	return borderF / (N / 2)
 
 
+@jit(float64(float64))
 def __calculate_borderF(deltaT: float) -> float:
 	"""
 	Calculate max frequency to be found depend on step of discretization
